941aa1ae5b68dcfeca8ed0e7604cd1e59f910191
Karsake <qianshangsam@gmail.com>
2019-05-18 17:34:34 +0800

    Update Game.ts

--- Client/assets/scripts/Scene/Game.ts
+++ Client/assets/scripts/Scene/Game.ts
@@ -224,6 +224,8 @@ export default class Game extends cc.Component {
         let ballNode = this._fallenBallsPool.get();
         ballNode.parent = this.fallenNode;
         ballNode.setPosition(data.node.x,data.node.y);
+        ballNode.color = data.node.color;
+        ballNode.getChildByName("score").getComponent(cc.Label).string = `${data.score}`;
         let randX = Math.random();
         let randY = Math.sqrt(1 - randX * randX);
         ballNode.getComponent(cc.RigidBody).linearVelocity = cc.v2(randX * 50,randY * 50);
